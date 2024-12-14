6bc69bd848dc4ee6c6eb3f23c0057e83dd6efd0c
Shubham Rawat <rawatshubam269@gmail.com>
2024-12-14 21:51:27 +0530

    Basic Index.js file running PORT

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-/* Server Setup Main File */
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Server is running on port 8082");
+});
+
+const PORT = 8082;
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
