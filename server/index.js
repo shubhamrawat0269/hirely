1ef94ed04f6f95dbc83e96c56c75c845e384d7e8
Shubham Rawat <rawatshubam269@gmail.com>
2024-12-15 11:38:11 +0530

    Added : Basic Server Setup Implemented

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
+require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
+const cookiesParser = require("cookie-parser");
 
 const app = express();
+const connectToDb = require("./db/connectToDb");
+const userRoutes = require("./routes/user.route");
 
-app.get("/", (req, res) => {
-  res.send("Server is running on port 8082");
-});
+app.use(cors());
+app.use(express.json());
+app.use(cookiesParser());
 
-const PORT = 8082;
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/user", userRoutes);
+
+connectToDb();
+
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
---
+++ server/schema/user.schema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profile: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = userSchema;
