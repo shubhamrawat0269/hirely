81c5eaf6aad59784ebdc468cb87380c8abaa9cb9
Shubham Rawat <rawatshubam269@gmail.com>
2025-01-12 15:55:54 +0530

    Added : Browse Page Section UI Done

--- client/src/pages/BrowseJobs.tsx
+++ client/src/pages/BrowseJobs.tsx
@@ -1,10 +1,21 @@
-import { Navbar, Footer } from '@/components/organisms'
+import { Navbar, Footer, Job } from '@/components/organisms'
+
+const randomJobs = [1, 2, 3, 4, 5, 6, 7]
 
 const BrowseJobs = () => {
   return (
     <div>
       <Navbar />
-      <h1>Browse Jobs section</h1>
+
+      <div className="max-w-7xl mx-auto my-10">
+        <h1 className="font-bold text-lg">Search Results ({randomJobs.length})</h1>
+
+        <div className="grid grid-cols-3 gap-4 my-5">
+          {randomJobs.map((job, index) => {
+            return <Job key={index} />
+          })}
+        </div>
+      </div>
       <Footer />
     </div>
   )
